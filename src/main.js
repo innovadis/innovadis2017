e210526690ee66aeb6373e79089c77385e88b060
Christiaan Maks <christilut@gmail.com>
2017-11-28 16:34:18 +0100

    polyfill

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+require('babel-polyfill')
+require('es6-promise').polyfill()
+require('es6-object-assign').polyfill()
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
