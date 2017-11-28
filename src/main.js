2a095543a35206357a07178c77e266c8184a4f55
Christiaan Maks <christilut@gmail.com>
2017-11-28 16:53:33 +0100

    babel

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import 'es6-promise/auto'
+// import 'es6-promise/auto'
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
