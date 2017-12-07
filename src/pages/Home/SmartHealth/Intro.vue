5dc423d1d9f186f2457f03bc4e52b48c9c0c03f9
Christiaan Maks <christilut@gmail.com>
2017-12-07 10:54:49 +0100

    minor

--- src/pages/Home/SmartHealth/Intro.vue
+++ src/pages/Home/SmartHealth/Intro.vue
@@ -1,9 +1,8 @@
 <template lang="pug">
-.intro.pt.flex.flex-justify-center.flex-align-center.flex-column
-  .container
-    h1 Wij verbinden,
-    h1.dot in de Smart Health
-    p.intro De zorg staat voor grote uitdagingen. De vergrijzing en daarmee toenemende druk op de zorg maakt veranderingen noodzakelijk. Wij geloven dat we door slim toepassen van moderne technologie de zorg kunnen verbeteren. En dan gaat het niet alleen om kostenbesparing maar zeker ook om het verbeteren van de kwaliteit van leven.
+.intro.pt.container
+  h1 Wij verbinden,
+  h1.dot in de Smart Health
+  p.intro De zorg staat voor grote uitdagingen. De vergrijzing en daarmee toenemende druk op de zorg maakt veranderingen noodzakelijk. Wij geloven dat we door slim toepassen van moderne technologie de zorg kunnen verbeteren. En dan gaat het niet alleen om kostenbesparing maar zeker ook om het verbeteren van de kwaliteit van leven.
 </template>
 
 <script>
@@ -15,18 +14,9 @@ export default {
 <style lang="scss" scoped>
 @import 'src/styles/variables';
 
-div.intro {
-
-  @include phablet {
-    padding-top: 100px;
-  }
-
+.intro {
   @include phone {
-    padding-top: 40px;
-  }
-
-  p {
-    margin-right: 100px;
+    padding-top: 0;
   }
 }
 </style>
--- src/pages/Home/SmartIndustry/Intro.vue
+++ src/pages/Home/SmartIndustry/Intro.vue
@@ -1,8 +1,7 @@
 <template lang="pug">
-.intro.pt.flex.flex-justify-center.flex-align-center.flex-column
-  .container
-    h1.dot Industrie en de mogelijkheden van technologie
-    p.intro De ontwikkelingen binnen de industrie gaan snel, zeker op het gebied van technologie en IT. Wat begon met een webshop of portaal, is nu een slimme online omgevingen waar bedrijven communiceren met afnemers, eindklanten, toeleveranciers en partners.
+.intro.pt.container
+  h1.dot Industrie en de mogelijkheden van technologie
+  p.intro De ontwikkelingen binnen de industrie gaan snel, zeker op het gebied van technologie en IT. Wat begon met een webshop of portaal, is nu een slimme online omgevingen waar bedrijven communiceren met afnemers, eindklanten, toeleveranciers en partners.
 </template>
 
 <script>
@@ -14,17 +13,9 @@ export default {
 <style lang="scss" scoped>
 @import 'src/styles/variables';
 
-div.intro {
-  @include phablet {
-    padding-top: 100px;
-  }
-
+.intro {
   @include phone {
-    padding-top: 40px;
-  }
-
-  p {
-    margin-right: 100px;
+    padding-top: 0;
   }
 }
 </style>
