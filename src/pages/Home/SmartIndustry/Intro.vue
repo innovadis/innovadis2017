74b9393acc8afff807fce83abf2ba209c35186be
Christiaan Maks <christilut@gmail.com>
2017-12-13 10:20:50 +0100

    move text

--- src/pages/Home/SmartIndustry/Intro.vue
+++ src/pages/Home/SmartIndustry/Intro.vue
@@ -2,6 +2,8 @@
 .intro.pt.container
   h1.dot Industrie en de mogelijkheden van technologie
   p.intro De ontwikkelingen binnen de industrie gaan snel, zeker op het gebied van technologie en IT. Wat begon met een webshop of portaal, is nu een slimme online omgevingen waar bedrijven communiceren met afnemers, eindklanten, toeleveranciers en partners.
+  p.intro Belangrijke thema’s zijn goede (product-) content, integratie met systemen, selfservice en personalisatie. Bovendien zijn steeds meer apparaten verbonden met het web, waardoor Internet of Things (IoT) en big data een belangrijkere rol gaat spelen. En vergeet ook de vraagstukken op het gebied van beveiliging niet. Dat noemen wij Smart Industry. Welkom in de wereld van Innovadis.
+
 </template>
 
 <script>
--- src/pages/Home/SmartIndustry/index.vue
+++ src/pages/Home/SmartIndustry/index.vue
@@ -3,7 +3,6 @@
   intro
 
   image-block(
-    text='Belangrijke thema’s zijn goede (product-) content, integratie met systemen, selfservice en personalisatie. Bovendien zijn steeds meer apparaten verbonden met het web, waardoor Internet of Things (IoT) en big data een belangrijkere rol gaat spelen. En vergeet ook de vraagstukken op het gebied van beveiliging niet. Dat noemen wij Smart Industry. Welkom in de wereld van Innovadis.',
     imageUrl='/static/images/cover/smartindustry.jpg'
     )
 
