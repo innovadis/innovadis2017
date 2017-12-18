1f03cbc1fca545f79ff71e923ed16d3cd85f6f2b
Christiaan Maks <christilut@gmail.com>
2017-12-18 13:33:25 +0100

    text

--- src/pages/Home/About/index.vue
+++ src/pages/Home/About/index.vue
@@ -3,7 +3,7 @@
   .top
     .container.pt.text-center
       h1.dot Wie wij zijn
-      p.intro Sinds 1994 bedenken- en bouwen we samen met onze opdrachtgevers innovatieve internetoplossingen. De kracht zit hem in onze teams die samen met onze opdrachtgevers de best mogelijke oplossing bedenkt die aansluit bij jullie strategie. Onze teams bestaan uit ervaren consultants, ontwikkelaars en designers die periodiek werkende software opleveren. Voorbeelden van onze oplossingen zijn webshops, portalen, PIM/ MDM, apps en maatwerk applicaties.
+      p.intro Sinds 1994 bedenken- en bouwen we samen met onze opdrachtgevers innovatieve internetoplossingen. Onze kracht zit in onze team-aanpak. Samen met onze opdrachtgevers bedenken en creëren we de best mogelijke oplossing, die aansluit bij hun strategie en doelen. Onze teams bestaan uit ervaren consultants, ontwikkelaars en designers die periodiek werkende software opleveren. Voorbeelden van onze oplossingen zijn webshops, portalen, PIM/ MDM, apps en maatwerk applicaties.
       p.intro We gaan voor duurzame relaties met al onze opdrachtgevers en partners. Opdrachtgevers waar we al meer dan 10 jaar mee samenwerken zijn geen uitzondering. Wij worden blij wanneer onze oplossingen mensen daadwerkelijk verder helpen en toegevoegde waarde leveren.
       p.intro Innovadis verbindt.
 
