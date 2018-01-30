7288b7bc1537a61c675d13d3af6fd1af68150cab
Jorien Kip <j.kip@innovadis.com>
2018-01-30 08:28:16 +0100

    privacy statement initialization

---
+++ src/pages/Home/PrivacyStatement/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+.page-conditions
+  .container.pt
+    h3.dot Privacy Statement
+    p Voor ons is een zorgvuldige omgang met persoonsgegevens van groot belang. Persoonlijke gegevens worden dan ook zorgvuldig verwerkt, beveiligd en vertrouwelijk behandeld. Innovadis de verantwoordelijke voor de gegevensverwerking. Omdat onze verwerkingen binnen de wettelijke vrijstellingsregelingen vallen, zijn deze niet aangemeld bij het College Bescherming Persoonsgegevens. In deze privacyverklaring leggen wij uit welke persoonsgegevens wij verzamelen en gebruiken en met welk doel. Deze privacyverklaring is voor het laatst aangepast op 01 februari 2018.
+    br
+    h3.dot ICT Office Voorwaarden
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'src/styles/variables';
+
+.page-conditions {
+  padding-bottom: 100px;
+
+}
+</style>
