4aabf2e4f50f9abd98c6eb5566352ccb82ea3c38
Christiaan Maks <christilut@gmail.com>
2018-04-23 14:10:48 +0200

    disable rm

--- build/build.js
+++ build/build.js
@@ -14,27 +14,27 @@ var fs = require('fs-extra')
 var spinner = ora('building for production...')
 spinner.start()
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+// rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
+//   if (err) throw err
+
+// TODO copy folder contents instead of manually
+fs.copySync(path.join(__dirname, '..', 'static/root/robots.txt'), path.join(__dirname, '..', 'dist/robots.txt'))
+
+webpack(webpackConfig, function (err, stats) {
+  spinner.stop()
   if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n\n')
 
-  // TODO copy folder contents instead of manually
-  fs.copySync(path.join(__dirname, '..', 'static/root/robots.txt'), path.join(__dirname, '..', 'dist/robots.txt'))
-
-  webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
-    if (err) throw err
-    process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n')
-
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
-  })
+  console.log(chalk.cyan('  Build complete.\n'))
+  console.log(chalk.yellow(
+    '  Tip: built files are meant to be served over an HTTP server.\n' +
+    '  Opening index.html over file:// won\'t work.\n'
+  ))
 })
+// })
